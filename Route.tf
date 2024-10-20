85bf5fe52b010ba406f6629671ade00dcaf050d8
anujramola <omiramola100@gmail.com>
2024-10-20 12:47:40 +0530

    Create Route for destination address using internet gateway and route table id

---
+++ Route.tf
@@ -0,0 +1,6 @@
+resource "aws_route" "internet_access" {
+    route_table_id = aws_route_table.AppRouteTable.id
+    gateway_id = aws_internet_gateway.AppIGW.id
+    destination_cidr_block = "0.0.0.0/0"
+  
+}
